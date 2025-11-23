942359bc042fab2d81a4b7a8f6967dd12e03157b
Khattab Ahmed <khattabz2050@gmail.com>
2025-11-24 02:20:50 +0300

    fix "lucide lucide-moon w-4 h-4

--- src/components/mode-toggler.tsx
+++ src/components/mode-toggler.tsx
@@ -3,15 +3,23 @@
 import { useTheme } from "next-themes"
 import { Button } from "./ui/button"
 import { MoonIcon, SunIcon } from "lucide-react"
+import { useEffect, useState } from "react"
 
 export default function ModeToggler() {
   const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
     <Button className="cursor-pointer" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
       {theme === 'dark' ? <SunIcon className="w-4 h-4" /> : <MoonIcon className="w-4 h-4" />}
     </Button>
-    
-
   )
 }
