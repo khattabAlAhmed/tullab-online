eda2f1b3b60936c7787c7768dd4b2bb82dd5e9a9
Khattab Ahmed <khattabz2050@gmail.com>
2025-12-05 15:51:56 +0300

    improve login signup pages

--- src/app/[locale]/(auth)/sign-in/page.tsx
+++ src/app/[locale]/(auth)/sign-in/page.tsx
@@ -1,15 +1,21 @@
 import { LoginForm } from "@/components/login-form";
-import { GalleryVerticalEnd } from "lucide-react";
+import { School2 } from "lucide-react";
+import { useLocale, useTranslations } from "next-intl";
+import Image from "next/image";
 
 export default function SignInPage() {
+  const t = useTranslations("Auth.SignIn");
+  const locale = useLocale()
+
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+      <div className="flex w-full max-w-sm flex-col gap-6 items-center">
+        <Image alt='logo' src={locale === 'ar' ? '/logo.png' : '/logo_english.png'} height={56.1} width={134.5} />
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+            <School2 className="size-4" />
           </div>
-          Focal Point
+          {t("title")}
         </a>
         <LoginForm />
       </div>
--- src/app/[locale]/(auth)/sign-up/page.tsx
+++ src/app/[locale]/(auth)/sign-up/page.tsx
@@ -1,16 +1,21 @@
-import { GalleryVerticalEnd } from "lucide-react"
+import { School2 } from "lucide-react"
 
 import { SignupForm } from "@/components/signup-form"
+import { useLocale, useTranslations } from "next-intl"
+import Image from "next/image"
 
 export default function SignupPage() {
+  const t = useTranslations("Auth.SignUp")
+  const locale = useLocale()
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+      <div className="flex w-full max-w-sm flex-col gap-6 items-center">
+        <Image alt='logo' src={locale === 'ar' ? '/logo.png' : '/logo_english.png'} height={56.1} width={134.5} />
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+            <School2 className="size-4" />
           </div>
-          Acme Inc.
+          {t("title")}
         </a>
         <SignupForm />
       </div>
